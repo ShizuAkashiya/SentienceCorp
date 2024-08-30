8c4e8a8f760ce8cd5b484b02fa66d7c4507c3bc0
ShizuAkashiya <bofis.ilp@gmail.com>
2024-08-30 21:05:10 +0700

    Fixed bug in login feature

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import mainIMG from '../assets/images/main-image.jpg';
 
 const Main = () => {
   return (
-    <section className="bg-cover bg-center bg-no-repeat bg-gray-800 text-white py-20" id="home" style={{ backgroundImage: `url('./src/assets/images/main-image.jpg')` }}>
-      <div className="container mx-auto flex flex-col md:flex-row items-center">
-        <div className="flex-1 text-center md:text-left">
-          <h1 className="text-5xl font-bold mb-6">Welcome to SentienceCorp</h1>
-          <p className="text-lg mb-6">Leading the future with innovative technology solutions.</p>
-          <a href="#features" className="bg-blue-600 text-white py-3 px-6 rounded-full hover:bg-blue-700">Learn More</a>
+    <section className="bg-gray-800 text-white py-20" id="home">
+      <div className="relative">
+        <img
+          src={mainIMG}
+          alt="Main Background"
+          className="absolute inset-0 w-full h-full object-cover"
+        />
+        <div className="container mx-auto relative z-10 flex flex-col md:flex-row items-center">
+          <div className="flex-1 text-center md:text-left">
+            <h1 className="text-5xl font-bold mb-6">Welcome to SentienceCorp</h1>
+            <p className="text-lg mb-6">Leading the future with innovative technology solutions.</p>
+            <a href="#features" className="bg-blue-600 text-white py-3 px-6 rounded-full hover:bg-blue-700">Learn More</a>
+          </div>
         </div>
       </div>
     </section>
